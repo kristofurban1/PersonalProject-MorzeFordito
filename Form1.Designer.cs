31de38453a6826b03cd4637d557ff4df0ce34548
Urbán Kristóf Tamás <kristofurban1@gmail.com>
2025-05-30 21:42:47 +0200

    ProjectCommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace MorzeFordito1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxSzoveg = new System.Windows.Forms.TextBox();
+            this.textBoxMorze = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioMorze = new System.Windows.Forms.RadioButton();
+            this.radioSzoveg = new System.Windows.Forms.RadioButton();
+            this.fordit = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxSzoveg
+            // 
+            this.textBoxSzoveg.Location = new System.Drawing.Point(19, 76);
+            this.textBoxSzoveg.Multiline = true;
+            this.textBoxSzoveg.Name = "textBoxSzoveg";
+            this.textBoxSzoveg.Size = new System.Drawing.Size(219, 347);
+            this.textBoxSzoveg.TabIndex = 0;
+            this.textBoxSzoveg.TextChanged += new System.EventHandler(this.textBoxSzoveg_TextChanged);
+            // 
+            // textBoxMorze
+            // 
+            this.textBoxMorze.Location = new System.Drawing.Point(430, 76);
+            this.textBoxMorze.Multiline = true;
+            this.textBoxMorze.Name = "textBoxMorze";
+            this.textBoxMorze.Size = new System.Drawing.Size(212, 347);
+            this.textBoxMorze.TabIndex = 1;
+            this.textBoxMorze.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Szöveg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(422, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 46);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Morze";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.radioMorze);
+            this.groupBox1.Controls.Add(this.radioSzoveg);
+            this.groupBox1.Location = new System.Drawing.Point(261, 307);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(140, 65);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " ";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Honnan fordít?";
+            // 
+            // radioMorze
+            // 
+            this.radioMorze.AutoSize = true;
+            this.radioMorze.Location = new System.Drawing.Point(89, 41);
+            this.radioMorze.Name = "radioMorze";
+            this.radioMorze.Size = new System.Drawing.Size(14, 13);
+            this.radioMorze.TabIndex = 1;
+            this.radioMorze.TabStop = true;
+            this.radioMorze.UseVisualStyleBackColor = true;
+            // 
+            // radioSzoveg
+            // 
+            this.radioSzoveg.AutoSize = true;
+            this.radioSzoveg.Location = new System.Drawing.Point(32, 41);
+            this.radioSzoveg.Name = "radioSzoveg";
+            this.radioSzoveg.Size = new System.Drawing.Size(14, 13);
+            this.radioSzoveg.TabIndex = 0;
+            this.radioSzoveg.TabStop = true;
+            this.radioSzoveg.UseVisualStyleBackColor = true;
+            // 
+            // fordit
+            // 
+            this.fordit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fordit.Location = new System.Drawing.Point(271, 167);
+            this.fordit.Name = "fordit";
+            this.fordit.Size = new System.Drawing.Size(124, 86);
+            this.fordit.TabIndex = 5;
+            this.fordit.Text = "Fordít";
+            this.fordit.UseVisualStyleBackColor = true;
+            this.fordit.Click += new System.EventHandler(this.fordit_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(772, 31);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Real time forditó";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Enabled = false;
+            this.textBoxInput.Location = new System.Drawing.Point(786, 58);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 7;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Enabled = false;
+            this.textBoxOutput.Location = new System.Drawing.Point(708, 84);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(253, 339);
+            this.textBoxOutput.TabIndex = 8;
+            this.textBoxOutput.TextChanged += new System.EventHandler(this.textBoxOutput_TextChanged);
+            this.textBoxOutput.DoubleClick += new System.EventHandler(this.textBoxOutput_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(756, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "<- translate back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(837, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Copy ->";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(551, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Copy ->";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(470, 429);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "<- Paste";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(892, 61);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(57, 17);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Space";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 487);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.fordit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMorze);
+            this.Controls.Add(this.textBoxSzoveg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Morze fordító";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSzoveg;
+        private System.Windows.Forms.TextBox textBoxMorze;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioMorze;
+        private System.Windows.Forms.RadioButton radioSzoveg;
+        private System.Windows.Forms.Button fordit;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace MorzeFordito1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            //no
+        }
+        private void textBoxOutput_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBoxTO_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void textBoxTO_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        public string[] abc = new string[54] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "\"", "&", "\'", ")", "(", ",", "-", "+", ".", "/", ":", ";", "=", "?", "@", "_", "*" };
+        public string[] ABC = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        public string[] morze = new string[54] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "--.--", ".-..-.", "...-..-", ".----.", "-.--.-", "-.--.", "--..--", "-....-", ".-.-.", ".-.-.-", "-..-.", "---...", "-.-.-.", "-...-", "..--..", ".--.-.", "..--.-", "-..-" };
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxSzoveg.Text = "";
+            textBoxMorze.Text = "";
+
+            radioSzoveg.Checked = true;
+        }
+        private void fordit_Click(object sender, EventArgs e)
+        {
+            if (radioSzoveg.Checked == true && textBoxSzoveg.Text != "" || radioMorze.Checked == true && textBoxMorze.Text != "")
+            {
+                errorProvider1.Clear();
+                if (radioSzoveg.Checked == true)
+                {
+                    //Szöveg
+                    textBoxMorze.Text = "";
+
+                    string selected = "";
+                    int pos = 0;
+                    string trans = "";
+                    string morze0 = "";
+
+                    for (int i = 0; i < textBoxSzoveg.TextLength; i++)
+                    {
+                        selected = textBoxSzoveg.Text.Substring(i, 1);
+
+                        if (abc.Contains(selected) == true)
+                        {
+                            pos = Array.IndexOf(abc, selected);
+
+                            trans = morze[pos].ToString() + " ";
+                        }
+                        else if (ABC.Contains(selected) == true)
+                        {
+                            pos = Array.IndexOf(ABC, selected);
+
+                            trans = morze[pos].ToString() + " ";
+                        }
+                        else
+                        {
+                            switch (selected)
+                            {
+                                case "á": trans = morze[0]; break;
+                                case "é": trans = morze[4]; break;
+                                case "í": trans = morze[8]; break;
+                                case "ó": trans = morze[14]; break;
+                                case "ö": trans = morze[14]; break;
+                                case "ő": trans = morze[14]; break;
+                                case "ú": trans = morze[20]; break;
+                                case "ü": trans = morze[20]; break;
+                                case "ű": trans = morze[20]; break;
+                                default: trans = "/"; break;
+                            }
+                            trans = trans + " ";
+
+                        }
+                        morze0 = morze0 + trans;
+                    }
+
+                    textBoxMorze.Text = morze0;
+                }
+                else
+                {
+                    //morze
+                    textBoxSzoveg.Text = "";
+
+                    int pos = 0;
+                    string trans = "";
+                    string szo = "";
+                    string text0 = "";
+
+                    string[] szavak = textBoxMorze.Text.Split('/');
+
+                    for (int i = 0; i < szavak.Length; i++)
+                    {
+                        string[] betuk = szavak[i].Split(' ');
+
+                        for (int x = 0; x < betuk.Length; x++)
+                        {
+                            if (betuk[x] != "")
+                            {
+                                if (morze.Contains(betuk[x]) == true)
+                                {
+                                    pos = Array.IndexOf(morze, betuk[x]);
+
+                                    trans = abc[pos];
+                                }
+                                else
+                                {
+                                    trans = " ";
+                                }
+                                szo = szo + trans;
+                            }
+                        }
+
+                        if (i != 0 || i != szavak.Length - 1)
+                        {
+                            szo = szo + " ";
+                        }
+
+                        text0 = text0 + szo;
+                        szo = "";
+
+                    }
+                    textBoxSzoveg.Text = text0;
+                }
+            }
+            else
+            {
+                errorProvider1.SetError(fordit, @"Irj valamit a kijelölt oldaltra!");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Realtime On?
+            if (checkBox1.Checked == true)
+            {
+                textBoxInput.Enabled = true;
+                textBoxOutput.Enabled = true;
+            }
+            else
+            {
+                textBoxInput.Enabled = false;
+                textBoxOutput.Enabled = false;
+                textBoxOutput.Text = "";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string rawText = textBoxInput.Text;
+
+            if (checkBox2.Checked != true)
+            {
+                int pos = -1;
+                string text = "";
+
+                if (abc.Contains(rawText) == true)
+                {
+                    pos = Array.IndexOf(abc, rawText);
+
+                    text = morze[pos].ToString() + " ";
+                }
+                else if (ABC.Contains(rawText) == true)
+                {
+                    pos = Array.IndexOf(ABC, rawText);
+
+                    text = morze[pos].ToString() + " ";
+                }
+                else
+                {
+                    switch (rawText)
+                    {
+                        case "á": text = morze[0]; break;
+                        case "é": text = morze[4]; break;
+                        case "í": text = morze[8]; break;
+                        case "ó": text = morze[14]; break;
+                        case "ö": text = morze[14]; break;
+                        case "ő": text = morze[14]; break;
+                        case "ú": text = morze[20]; break;
+                        case "ü": text = morze[20]; break;
+                        case "ű": text = morze[20]; break;
+                        case " ": text = "/"; break;
+                        default:; break;
+                    }
+                    text = text + " ";
+                }
+                textBoxOutput.Text = textBoxOutput.Text + text;
+                textBoxInput.Text = "";
+            }
+            else
+            {
+                if (rawText.Substring(rawText.Length - 1, 1) == " ")
+                {
+                    int pos = -1;
+                    string text = "";
+
+                    if (abc.Contains(rawText) == true)
+                    {
+                        pos = Array.IndexOf(abc, rawText);
+
+                        text = morze[pos].ToString() + " ";
+                    }
+                    else if (ABC.Contains(rawText) == true)
+                    {
+                        pos = Array.IndexOf(ABC, rawText);
+
+                        text = morze[pos].ToString() + " ";
+                    }
+                    else
+                    {
+                        switch (rawText)
+                        {
+                            case "á": text = morze[0]; break;
+                            case "é": text = morze[4]; break;
+                            case "í": text = morze[8]; break;
+                            case "ó": text = morze[14]; break;
+                            case "ö": text = morze[14]; break;
+                            case "ő": text = morze[14]; break;
+                            case "ú": text = morze[20]; break;
+                            case "ü": text = morze[20]; break;
+                            case "ű": text = morze[20]; break;
+                            case " ": text = "/"; break;
+                            default:; break;
+                        }
+                        text = text + " ";
+                    }
+                    textBoxOutput.Text = textBoxOutput.Text + text;
+                    textBoxInput.Text = "";
+                }
+            }
+        }
+
+        private void textBoxOutput_DoubleClick(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxMorze.Text = "";
+            textBoxMorze.Text = textBoxOutput.Text;
+            fordit_Click(sender, e);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            radioMorze.Checked = true;
+        }
+
+        private void textBoxSzoveg_TextChanged(object sender, EventArgs e)
+        {
+            radioSzoveg.Checked = true;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBoxOutput.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBoxMorze.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBoxMorze.Text = Clipboard.GetText();
+        }
+    }
+}
